adf9ffdd06dee58794f79dc6ac75008890865dfa
freemaan90 <fredericozanoni@gmail.com>
2026-03-29 00:52:13 -0300

    crud de usuario completo

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserModel } from 'src/generated/prisma/models';
 import { UpdateUserDto, UserCreateDto, UserResponseDto } from './dto/user.dto';
@@ -10,6 +10,10 @@ export class UserController {
   @Post('new')
   async signupUser(@Body() userData: UserCreateDto): Promise<UserModel> {
     return this.userService.createUser(userData);
+  }
+  @Get(`:id`)
+  async getUser(@Param(`id`) id:string){
+    return this.userService.user({id: Number(id)})
   }
   @Get('all-users')
   async getUsers() {
@@ -23,5 +27,10 @@ export class UserController {
       where: { id: Number(id) },
       data,
     });
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: number) {
+    return this.userService.deleteUser({ id: Number(id) });
   }
 }
